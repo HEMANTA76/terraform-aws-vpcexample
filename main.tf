887ed3a3f27f2f4d0c7b6ce93540103b5f9cae12
Hemanta Kumar Nayak <hemanta1.nayak@gmail.com>
2024-08-31 18:03:30 -0400

    subnet configuration added

--- main.tf
+++ main.tf
@@ -26,6 +26,20 @@ resource "aws_vpc" "vpc" {
   )
 }
 
+resource "aws_subnet" "subnets" {
+  for_each = var.subnets
+
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.availability_zone
+  map_public_ip_on_launch = each.value.public
+
+  tags = merge(
+    { Name = each.key },
+    local.tags
+  )
+}
+
 resource "aws_internet_gateway" "igw" {
   count = var.enable_internet_gatway ? 1 : 0
 }
@@ -36,6 +50,14 @@ resource "aws_internet_gateway_attachment" "igw" {
 }
 locals {
   ngw_subnets = [
-    for k, v in var.subnets : aws_subnets.subnets[k].id if v.public
+    for k, v in var.subnets : aws_subnet.subnets[k].id if v.public
   ]
+}
+
+resource "aws_nat_gateway" "ngw" {
+  depends_on = [aws_internet_gateway.igw[0]]
+  for_each   = toset(local.ngw_subnets)
+
+  connectivity_type = "private"
+  subnet_id         = each.value.key
 }
